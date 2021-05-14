2fb3661d1039607c97af76828d417be1271bb1bc
saber <duongnd041198@gmail.com>
2021-05-14 08:50:21 +0700

    update mapping

--- BE/BIDVTool/src/main/java/com/ngs/response/BaseResponse.java
+++ BE/BIDVTool/src/main/java/com/ngs/response/BaseResponse.java
@@ -4,7 +4,6 @@ package com.ngs.response;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Data
 @Builder
--- BE/BIDVTool/src/main/java/com/ngs/response/ImportExcelResponseBody.java
+++ BE/BIDVTool/src/main/java/com/ngs/response/ImportExcelResponseBody.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImportExcelResponseBody {
-
     private String status;
     private int totalRecord;
 
