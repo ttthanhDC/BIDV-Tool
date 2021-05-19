57c6135abf35be72efeddf645dffcd3c6911c72c
Dejones1109 <tranvande19980911@gmail.com>
2021-05-19 17:32:42 +0700

    add

--- BE/BIDVTool/src/main/java/com/ngs/request/CreateTaskRequest.java
+++ BE/BIDVTool/src/main/java/com/ngs/request/CreateTaskRequest.java
@@ -1,4 +1,21 @@
 package com.ngs.request;
 
+import com.ngs.entity.Operation;
+import com.ngs.entity.User;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+@Data
 public class CreateTaskRequest {
+
+    private String description;
+    private Integer userId;
+    private Integer operationId;
+    private String mappingSheet;
+    private Date openDate;
+    private Date dueDate;
+    private Date closeDate;
+    private String status;
+
 }
